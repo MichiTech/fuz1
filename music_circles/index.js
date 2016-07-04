af6882ce41a555051d33e758688e6765f9b5235f
MichiTech <michifuz04@gmail.com>
2016-07-04 12:06:15 -0700

    Add files via upload

---
+++ music_circles/index.js
@@ -0,0 +1,35 @@
+
+var audio = document.getElementById("cAudio"); 
+function playclip() { 
+cAudio.play(); 
+}
+ 
+var audio = document.getElementById("dAudio"); 
+function playclip1() { 
+dAudio.play(); 
+}
+
+var audio = document.getElementById("eAudio"); 
+function playclip2() { 
+eAudio.play(); 
+}
+   
+var audio = document.getElementById("fAudio"); 
+function playclip3() { 
+fAudio.play(); 
+}
+    
+var audio = document.getElementById("gAudio"); 
+function playclip4() { 
+gAudio.play(); 
+}
+   
+var audio = document.getElementById("aAudio"); 
+function playclip5() { 
+aAudio.play(); 
+}
+ 
+var audio = document.getElementById("bAudio"); 
+function playclip6() { 
+bAudio.play(); 
+}
